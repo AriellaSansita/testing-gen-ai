db20d991f86ce50d35dc6af8e82196bf1744c2b0
Ariella Sansita <ariella.sansita@gmail.com>
2026-01-07 10:16:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
-import google.genai
+from google import genai
+
 try:
-   client = google.genai.Client()
-  st.write("Gemini API client configured.")
-   prompt = "Benefits of API key management?"
-   response = client.models.generate_content(
-      model="gemini-2.5-flash",
-      contents=prompt
-   )
-  st.write(f"Response: {response.text}")
+    client = genai.Client(api_key=st.secrets["AIzaSyBhwR6bbd5t4neTkSVB3i3DHqYFyA_Zy-w"])
+    st.write("Gemini API client configured.")
+
+    prompt = "Benefits of API key management?"
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    st.write(f"Response: {response.text}")
+
 except Exception as e:
-  st.error(f"Error: {e}. Check your 'GEMINI_API_KEY' in secrets.")
-
-
+    st.error(f"Error: {e}. Check your 'GEMINI_API_KEY' in secrets.")
