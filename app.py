13738e01ae10b913efd9244e429926c4631c8414
Ariella Sansita <ariella.sansita@gmail.com>
2026-01-07 10:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
 
-user_input = st.chat_input("Type your message")
+user_input = st.chat_input("Type a message")
 
 if user_input:
     st.session_state.messages.append(
         {"role": "user", "content": user_input}
     )
+
     with st.chat_message("user"):
         st.markdown(user_input)
 
@@ -27,14 +28,14 @@ if user_input:
         contents=user_input
     )
 
-    reply = response.text
+    reply = response.text.strip()
 
     st.session_state.messages.append(
         {"role": "assistant", "content": reply}
     )
+
     with st.chat_message("assistant"):
         st.markdown(reply)
-
 
 
 """
