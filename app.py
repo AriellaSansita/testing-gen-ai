bb1c78e287a8c641379e5bfb28cc346dceb3a2ca
Ariella Sansita <ariella.sansita@gmail.com>
2026-01-07 10:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,30 +36,3 @@ if user_input:
 
     with st.chat_message("assistant"):
         st.markdown(reply)
-
-
-"""
-import streamlit as st
-from google import genai
-
-while True:
-   user_input = input("You: ")
-   if user_input.lower() == "exit":
-       print("Bot: Goodbye!")
-   break
-
-try:
-    client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
-    st.write("Gemini API client configured.")
-
-    prompt = "Benefits of API key management?"
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=user_input
-    )
-
-    st.write(f"Response: {response.text}")
-
-except Exception as e:
-    st.error(f"Error: {e}. Check your 'GEMINI_API_KEY' in secrets.")
-"""
